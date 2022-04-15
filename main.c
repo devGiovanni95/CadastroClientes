9eee0a01ff47ff56fcd4d603cded194366b23436
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2022-04-15 20:18:13 -0300

    Add files via upload
    Creating Function for Open and Close Files

--- main.c
+++ main.c
@@ -2,8 +2,40 @@
 #include <stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
+//colocamos como file* pois é esperado um retorno do tipo txt
+//definimos tambem o modo que é leitura gravação append
+//e o caminho ate o arquivo
+FILE* AbreArquivo(char modo, char caminho[30]){
+	FILE *arquivo;
+	switch(modo){
+		case 'g':
+			arquivo = fopen(caminho,"wt");
+			break;
+		case 'l':
+			arquivo = fopen(caminho,"rt");
+			break;
+		case 'a':
+			arquivo = fopen(caminho,"a");
+			break;
+	}
+	//se houver algum erro, o ponteiro apontará para null
+	if(arquivo==NULL){
+		printf("Não foi possivel abrir o caminho");
+		exit(0);
+	}
+	return arquivo;
+	
+}
+
+//funcao com parametro indicativo do tipo de arquivo
+void FecharArquivo(FILE *arquivo){
+	//fechar o arquivo.txt
+	fclose(arquivo);
+}
+
 int main() {
-	char vetor[4];
+	char vetor[6];
 	int valor;
 
 	//definindo ponteiro para abrir um arquivo
@@ -11,41 +43,25 @@ int main() {
 
 
 	//Abrir arquivo
-	//primeira aspa local do arquivo
-	//modo abertura do arquivo
-	//wt = escrita
-	//rt = leitura
-	//a = append
-	//usando o wt se nao existir esse arquivo ele vai criar um novo com o nome definido
-	arquivo = fopen("teste.txt", "wt");
-	
-	//se houver algum erro, o ponteiro apontará para null
-	if (arquivo == NULL) {
-		printf("Não foi possivel abrir o arquivo");
-		exit(0);
+	arquivo = AbreArquivo('g', "teste.txt");
 
-	}
 	
 	//escreve dados no arquivo de txt
-	fprintf(arquivo,"Ola 123");
+	fprintf(arquivo,"teste 78965");
 
-//Fecha o arquivo
-	fclose(arquivo);
+	//Fecha o arquivo
+	FecharArquivo(arquivo);
 	
 	
 	//Abrindo o arquivo em modo leitura
-	arquivo = fopen("teste.txt", "rt");
-	
-	//se houver algum erro, o ponteiro apontará para null
-	if (arquivo == NULL) {
-		printf("Não foi possivel abrir o arquivo");
-		exit(0);
-	}
+	arquivo = AbreArquivo('l', "teste.txt");
 	
 	fscanf(arquivo,"%s %d",&vetor,&valor);
-	printf("\n\n%s \n\n%d",vetor,valor);
+	printf("\n\n%s %d\n\n",vetor,valor);
 	
-
+	//Fecha o arquivo
+	FecharArquivo(arquivo);
+	
 	system("pause");
 	return 0;
 }
